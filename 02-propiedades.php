b172a74d06cf528259eee52c61f7c75a556851a0
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2020-10-30 14:29:25 -0300

    php poo

---
+++ 02-propiedades.php
@@ -0,0 +1,15 @@
+<?php include 'includes/header.php';
+
+class MenuRestaurant {  
+    public $nombre = 'nombre del producto';
+    public $precio = 0; 
+     
+}
+
+$bebida = new MenuRestaurant();
+$postre = new MenuRestaurant();
+
+var_dump($bebida);
+var_dump($postre);
+
+    
